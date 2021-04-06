ebb1d156de975af01ab8b28cba34f9bbb08008f8
Johar Ali <50805982+JoharPiaic@users.noreply.github.com>
2021-04-07 00:37:32 +0500

    Deploy nested

---
+++ app.js
@@ -0,0 +1,27 @@
+
+for(a=0; a<=5; a++)
+{
+    for(b=0; b<=a; b++){
+
+        document.write(b + " ")
+    }
+    document.write( a + "<br> ")
+}
+
+
+
+// for(a=0; a<=5; a++)
+// {
+//     for(b=0; b<=a; b++){
+
+//         document.write(b + " ")
+//     }
+//     document.write(b+ "<br> ")
+// }
+
+// 0 1
+// 0 1 2
+// 0 1 2 3
+// 0 1 2 3 4
+// 0 1 2 3 4 5
+// 0 1 2 3 4 5 6
